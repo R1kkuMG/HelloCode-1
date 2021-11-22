6e3d0e7f250c9d09a2af4b6329fe775cec7b7fa0
Maggie <alis-weasley@mail.ru>
2021-11-22 21:36:36 +0300

    add task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -4,5 +4,7 @@ int y = new Random().Next(1, 10); // 1 2 3 4 ... 9
 Console.WriteLine(y);
 int z = new Random().Next(1, 10); // 1 2 3 4 ... 9
 Console.WriteLine(z);
+int result = x * y + z;
 
-Console.WriteLine(x * y + z);
+Console.Write("a = ");
+Console.Write(result);
