0a55c2a15332e75723d56fc4116bd8b1d529e0f4
Maggie <alis-weasley@mail.ru>
2021-11-25 21:45:56 +0300

    add task11

---
+++ task11/Program.cs
@@ -0,0 +1,16 @@
+﻿int numA = new Random().Next(10, 100); // 1 2 3 4 ... 9
+Console.WriteLine(numA);
+
+int a = numA/10;
+int b = numA%10;
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
