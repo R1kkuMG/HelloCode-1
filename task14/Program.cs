7cfa9a98930293bf58d6f4c4f0b84adaa75aee34
Maggie <alis-weasley@mail.ru>
2021-11-25 22:47:37 +0300

    correct task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,12 +1,13 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
 
-Console.Write("Введите трехзначное число: ");
+Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 Console.WriteLine(num);
-int A = num % 100;
-if(x > 1)
+//int A = num % 100;
+if(num / 10 > 1)
 {
-    
+    int B = num % 10;
+    Console.WriteLine(B);
 }
 else
 {
