ec477983ae2d92db3e469bb31ca055a81bf8f5b1
Maggie <alis-weasley@mail.ru>
2021-12-09 12:04:19 +0300

    add task20

---
+++ task20/Program.cs
@@ -0,0 +1,21 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти координат:");
+int Q = int.Parse(Console.ReadLine());
+
+if(Q == 1)
+{
+    Console.WriteLine("X = (0; +∞) Y = (0; +∞)");
+}
+if(Q == 2)
+{
+    Console.WriteLine("X = (0; -∞) Y = (0; +∞)");  
+}
+if(Q == 3)
+{
+    Console.WriteLine("X = (0; -∞) Y = (0; -∞)");  
+}
+if(Q == 4)
+{
+    Console.WriteLine("X = (0; +∞) Y = (0; -∞)");  
+}
