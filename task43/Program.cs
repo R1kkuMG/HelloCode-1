2a222e765a90434dcb4df4ef7d6c22165f29a70c
Maggie <alis-weasley@mail.ru>
2021-12-13 11:11:31 +0300

    add corrected task43

--- task43/Program.cs
+++ task43/Program.cs
@@ -1,7 +1,7 @@
 ﻿// 43. Написать программу преобразования десятичного числа в двоичное
 
 Console.Clear();
-int a = new Random().Next(0, 255);
+int a = new Random().Next(0, 256);
 Console.WriteLine(a);
 string b = string.Empty;
 while(a > 0)
