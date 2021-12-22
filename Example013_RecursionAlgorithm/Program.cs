e9047963f5cbb8a767563a882c71a86c3360fd28
Maggie <alis-weasley@mail.ru>
2021-12-22 13:22:10 +0300

    add new solution for recursion' :wq

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -150,3 +150,21 @@ for (int i = 1; i < 40; i++)
 // {
 //     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
 // }
+
+
+// перебор слов
+int n = 1;
+void FindWords(string alphabet, char[] word, int length =0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length +1);
+    }
+}
+FindWords("аисв", new char[2]);
